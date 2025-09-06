fb95356645d20cfe6bb122a5a2eeed2a04e0e9a1
Actlysis <bellomubarak123456@gmail.com>
2025-09-06 14:26:09 +0100

    Delete exercise 01 directory

--- exercise 01/answer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-contract A{
-    uint public variable = 991;
-    function modifyvariable(uint changevariable) external {
-        variable = changevariable;
-    }
-}

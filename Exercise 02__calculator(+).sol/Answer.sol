2e99428a3d64bdc723b703c22857a55cec504c49
Actlysis <bellomubarak123456@gmail.com>
2025-09-13 03:12:42 +0100

    Update Answer.sol

--- Exercise 02__calculator(+).sol/Answer.sol
+++ Exercise 02__calculator(+).sol/Answer.sol
@@ -1,16 +1,1 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-contract A{
-   function add(uint x , uint y) external pure returns(uint){ 
-    return (x + y);}
-    function subtract(uint x , uint y) external pure returns(uint){
-        return(x - y);
-    }
-    function multiply(uint x , uint y) external pure returns(uint){
-        return(x * y);
-    }
-    function divide(uint x , uint y) external pure returns(uint){
-        return x / y;
-    }
-   }
+e
